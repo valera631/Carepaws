2d5588d449b4a9026b3f51ea23dad3455bd870f7
Valera <valera63806@gmail.com>
2024-09-01 18:26:47 +0300

    ID-B-3

---
+++ backend/src/CarePaws.Domain/Volunteer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarePaws.Domain
+{
+    public class Volunteer
+    {
+        public Guid Id { get; set; }
+
+
+    }
+}
